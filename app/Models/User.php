a9b6b9ed9fb2b8e51746b30bdad471d58cd1e2fd
walubo <magtoaries799@gmail.com>
2026-02-14 00:37:36 +0800

    Create User model file

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = ['name', 'email', 'password'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+}
